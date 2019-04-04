cec91d887bdd97d095e819265a74dd428d9608b9
jessicaschelly <jessica_schelly@hotmail.com>
2019-04-04 12:32:37 -0300

    entidades

---
+++ SistemadeCompra/src/entidades/Funcionario.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+public class Funcionario {
+
+    String nome;
+    String ID;
+
+    public Funcionario(String nome, String ID) {
+        this.nome = nome;
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+}
